95e96b247fa6f76a4e2ec072fe5f1d337ab3ac16
d4t4king <dataking@gmail.com>
2014-09-17 20:42:58 +0000

    Minor tweaks.

--- lynis-harden.sh
+++ lynis-harden.sh
@@ -9,20 +9,20 @@ if [ $(id -u) != 0 ]; then
 	echo "This script must be run as root.\n";
 fi
 
-# update first, since lynis checks for that.
-apt-get update
-apt-get upgrade -y
-
 # some basic package configuration
- for F in `find / -type f -name "php.ini"`; do
+# php.ini's
+for F in `find / -type f -name "php.ini"`; do
 	echo -n "${F}" && sed -i -e 's/\(expose_php = \)On/\1Off/' -e 's/\(allow_url_fopen = \)On/\1Off/' $F
 	#echo -n "${F}" && sed -e 's/\(allow_url_fopen = \)On/\1Off/' $F
 done
+# postfix banner obfuscation
 sed -i -e 's/\(smtpd_banner = \$myhostname ESMTP\) $mail_name (Ubuntu)/\1/' /etc/postfix/main.cf
+/etc/init.d/postfix reload
+# default umasks
 sed -i -e 's/\(UMASK.*\?\)022/\1027/' /etc/login.defs
 sed -i -e 's/\(umask\) 022/\1 027/' /etc/init.d/rc
 
 # update first
 apt-get update && apt-get upgrade -y
 # install some required packages
-#apt-get install libpam-cracklib clamav aide apt-show-versions -y
+apt-get install libpam-cracklib clamav aide apt-show-versions -y
