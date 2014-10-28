4ad15a710c8c06a6fb5d84e4463dd2a7e8893022
Charlie Heselton <charlie@dataking.us>
2014-10-28 17:41:26 +0000

    Modified to delete local archive after successful transfer.

--- daily-backup-dkt.sh
+++ daily-backup-dkt.sh
@@ -4,6 +4,9 @@ DATE=$(date '+%Y-%m-%d')
 tar cvpfJ /root/www.dataking.technology-${DATE}.tar.xz --directory /root/ /usr/share/nginx/html/ /root/www.dataking.technology/
 if [ $? -eq 0 ]; then
 	scp -P 2222 /root/www.dataking.technology-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
+	if [ $? -eq 0 ]; then
+		rm -f /root/www.dataking.technology-${DATE}.tar.xz
+	fi
 else
 	echo "There was a problem with the backup."
 fi
