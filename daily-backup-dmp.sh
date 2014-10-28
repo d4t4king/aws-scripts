0ca17255d2564094aed61b5ddde1ef5ea817084b
root@www.diegominpin.com <root@dataking.us>
2014-10-28 17:32:42 +0000

    Modified to remove local file after transferring offsite.

--- daily-backup-dmp.sh
+++ daily-backup-dmp.sh
@@ -4,6 +4,9 @@ DATE=$(date '+%Y-%m-%d')
 tar cvpfJ /root/www.diegominpin.com-${DATE}.tar.xz --directory /root/ --exclude /usr/share/nginx/html/gallery/resources/cache --exclude /root/www.diegominpin.com/gallery/resources/cache/ /usr/share/nginx/html/ /root/www.diegominpin.com/
 if [ $? -eq 0 ]; then
 	scp -P 2222 /root/www.diegominpin.com-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
+	if [ $? -eq 0 ]; then
+		rm -f /root/www.diegominpin.com-${DATE}.tar.xz
+	fi
 else
 	echo "There was a problem with the backup."
 fi
