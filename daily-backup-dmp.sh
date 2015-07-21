67a5cf0db406d4b0d4fd9bdca61ea8a3ad5e31f5
root@www.diegominpin.com <root@dataking.us>
2015-07-21 00:25:08 +0000

    Minor tweak to dmp backup script.  Copies backups to S3.

--- daily-backup-dmp.sh
+++ daily-backup-dmp.sh
@@ -3,9 +3,12 @@
 DATE=$(date '+%Y-%m-%d')
 tar cvpfJ /root/www.diegominpin.com-${DATE}.tar.xz --directory /root/ --exclude /usr/share/nginx/html/gallery/resources/cache --exclude /root/www.diegominpin.com/gallery/resources/cache/ /usr/share/nginx/html/ /root/www.diegominpin.com/
 if [ $? -eq 0 ]; then
-	scp -P 2222 /root/www.diegominpin.com-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
+	#scp -P 2222 /root/www.diegominpin.com-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
+	aws s3 cp /root/www.diegominpin.com-${DATE}.tar.xz s3://dk-website-backups/diegominpin.com/
 	if [ $? -eq 0 ]; then
 		rm -f /root/www.diegominpin.com-${DATE}.tar.xz
+	else
+		echo "There was a problem with the transfer."
 	fi
 else
 	echo "There was a problem with the backup."
