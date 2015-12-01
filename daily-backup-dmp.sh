ad69b5294fb1a2d069dce0ed10a553d1302be20a
root@www.diegominpin.com <root@dataking.us>
2015-12-01 20:29:07 +0000

    Added imgconvert.sh and updated dmp backup script.

--- daily-backup-dmp.sh
+++ daily-backup-dmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/www.diegominpin.com-${DATE}.tar.xz --directory /root/ --exclude /usr/share/nginx/html/gallery/resources/cache --exclude /root/www.diegominpin.com/gallery/resources/cache/ /usr/share/nginx/html/ /root/www.diegominpin.com/ /etc/nginx/
+tar cvpfJ /root/www.diegominpin.com-${DATE}.tar.xz --directory /root/ --exclude /usr/share/nginx/html/gallery/resources/cache --exclude /root/www.diegominpin.com/gallery/resources/cache/ /usr/share/nginx/html/ /root/www.diegominpin.com/ /etc/nginx/ /root/img_backup/
 if [ $? -eq 0 ]; then
 	#scp -P 2222 /root/www.diegominpin.com-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
 	aws s3 cp /root/www.diegominpin.com-${DATE}.tar.xz s3://dk-website-backups/diegominpin.com/
---
+++ imgconvert.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#GALLERY_DIR='/usr/share/nginx/html/gallery'
+BACKUP_DIR='/root/img_backup'
+
+GALLERY_DIR="$1"
+
+for P in `ls -1 "$GALLERY_DIR"`; do
+	EXT=$(echo $P | awk -F. '{ print $NF }')
+	#echo "EXT: $EXT"
+	if ! [[ "$EXT" =~ (jpg|png|gif|tiff|bmp) ]]; then continue; fi
+	echo -n "$P:	"
+	RES=`exiftool "$GALLERY_DIR/$P" | grep 'Image Size' | awk '{ print $4 }'`
+	case $RES in
+		"456x256")
+			echo "Small Portrait"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 1024x576 "$GALLERY_DIR/$P"
+			;;
+		"256x455")
+			echo "Small Landscape"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 576x1024 "$GALLERY_DIR/$P"
+			;;
+		"1593x1195")
+			echo "Medium Phone Landscape (Horizontal)"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 64% "$GALLERY_DIR/$P"
+			;;
+		"5809x3873")
+			echo "Enormous Landscape (Horizontal)"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 20% "$GALLERY_DIR/$P"
+			;;
+		"6000x4000")
+			echo "Enormous Landscape (Horizontal)"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 20% "$GALLERY_DIR/$P"
+			;;
+		"4000x6000")
+			echo "Enormous Portrait (Vertical)"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 20% "$GALLERY_DIR/$P"
+			;;
+		"5312x2988")
+			echo "X-Large Phone Landscape (Horizontal)"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 20% "$GALLERY_DIR/$P"
+			;;
+		"2988x5312")
+			echo "X-Large Phone Portrait (Vertical)"
+			cp -vr "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 20% "$GALLERY_DIR/$P"
+			;;
+		"2448x3264")
+			echo "Large portrait!"
+			cp -vf "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 768x1024 "$GALLERY_DIR/$P"
+			;;
+		"3264x2448")
+			echo "Large landscape!"
+			cp -vf "$GALLERY_DIR/$P" "$BACKUP_DIR/$P"
+			convert "$BACKUP_DIR/$P" -resize 1024x768 "$GALLERY_DIR/$P"
+			;;
+		*)
+			echo " ${RES}"
+			;;
+	esac
+done
