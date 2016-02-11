dcce6a373a028913ac7347c74262c9bf12b29c33
dataking <dataking@gmail.com>
2016-02-11 18:47:35 -0500

    Minor tweaks

--- daily-backup-do2.sh
+++ daily-backup-do2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/do2.dataking.us-${DATE}.tar.xz --directory /root/ /var/www/html/ /etc/nginx/
+tar cvpfJ /root/do2.dataking.us-${DATE}.tar.xz --directory /root/ /var/www/html/ /usr/share/nginx/html /etc/nginx/
 if [ $? -eq 0 ]; then
 	#scp -P 2222 /root/www.dataking.us-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
 	aws s3 cp /root/do2.dataking.us-${DATE}.tar.xz s3://dk-website-backups/do2.dataking.us/
