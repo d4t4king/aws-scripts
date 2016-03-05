cb1f754dc27b5b631e8370eb75b02d1b119e8ef9
d4t4king <dataking@gmail.com>
2016-03-05 08:26:38 +0000

    New backup script for Sherman.  ;-)

---
+++ daily-backup-sts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATE=$(date '+%Y-%m-%d')
+tar cvpfJ /root/shaermantheshermanator.com-${DATE}.tar.xz --directory /root/ /var/www/shermantheshermanator.com/
+if [ $? -eq 0 ]; then
+	#scp -P 2222 /root/www.dataking.us-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
+	aws s3 cp /root/shermantheshermanator.com-${DATE}.tar.xz s3://dk-website-backups/shermantheshermanator.com/
+	if [ $? -eq 0 ]; then
+		rm -f /root/shermantheshermanator.com-${DATE}.tar.xz
+	else
+		echo "There was a problem with the transfer."
+	fi
+else
+	echo "There was a problem with the backup."
+fi
