45a46090c2ce8404832a69a6db26d60bb6875517
dataking <dataking@gmail.com>
2016-03-23 00:21:45 -0700

    Minor tweaks.

--- perl-template.pl
+++ perl-template.pl
@@ -5,22 +5,27 @@ use warnings;
 
 use Geo::IP::PurePerl;
 use Term::ANSIColor;
-use Getopt::Long;
-my ($help, $nocolor);
+use Getopt::Long qw( :config no_ignore_case bundling );
+my ($help, $nocolor, $verbose);
+$verbose = 0;
 GetOptions(
 	'h|help'		=>	\$help,
-	'nc|no-color'	=>	\$nocolor,
+	'v|verbose+'		=>	\$verbose,
+	'nc|no-color'		=>	\$nocolor,
 );
 
 sub Usage() {
 	print <<EOF;
 
-$0 [-h] [-nc]
+$0 [-h] [-v] [-nc]
 
 -h	|	--help			Displays this help message.
+-v	|	--verbose		Display more output.  More repetitions increases verbosity.
 -nc	|	--no-color		Turns off colorized text.
 
 EOF
+
+	return 1;			# return true! (if perl had booleans)
 }
 
 if ($help) { &Usage(); }
