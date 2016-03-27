d78072611d3d50d28fbce156763a09fa443f5fb0
dataking <dataking@gmail.com>
2016-03-26 17:06:24 -0700

    Tweaks for new systems

--- backup_local.sh
+++ backup_local.sh
@@ -17,8 +17,8 @@ if [ "${1}x" == "homex" ]; then
 elif [ "${1}x" == "varx" ]; then
 	TARBALL="/tmp/varetc_${DATE}_${HOSTNAME}.tar.xz"
 	echo $TARBALL
-	if [ "${HOSTNAME}" == "mars" ]; then 
-		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/localhost/htdocs/mirror --exclude /var/www/localhot/htdocs/isos /var /etc
+	if [ "${HOSTNAME}" == "luna" ]; then 
+		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/html/mirror --exclude /var/www/html/isos /var /etc
 	elif [ "${HOSTNAME}" == "mercury.dataking.us" ]; then
 		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/html/mirror --exclude /var/www/html/isos /var /etc
 	else
@@ -27,7 +27,7 @@ elif [ "${1}x" == "varx" ]; then
 else
 	# Skip any VMs for the full backup.  We should have gotten them in the "home" backup.
 	TARBALL="/tmp/full_${DATE}_${HOSTNAME}.tar.xz"
-	if [ "${HOSTNAME}" == "mars" ]; then
+	if [ "${HOSTNAME}" == "luna" ]; then
 		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /s /
 	else
 		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" /
@@ -39,7 +39,7 @@ if [ $HOSTNAME == "mercury.dataking.us" ]; then
 fi
 if [ "${HOSTNAME}" == "jupiter.dataking.us" -o "${HOSTNAME}" == "neptune.dataking.us" ]; then
 	scp ${TARBALL} 192.168.100.5:/opt/backups/${HOSTNAME}/
-elif [ "${HOSTNAME}" == "mars" ]; then
+elif [ "${HOSTNAME}" == "luna" ]; then
 	scp ${TARBALL} oortcloud:/opt/backups/${HOSTNAME}.dataking.us/
 else
 	scp ${TARBALL} oortcloud:/opt/backups/${HOSTNAME}/
