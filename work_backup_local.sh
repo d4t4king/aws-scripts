bf6c2481ff96c1c0bed513735e77957879747acf
dataking <dataking@gmail.com>
2016-05-11 15:52:42 -0700

    Minor tweaks

--- work_backup_local.sh
+++ work_backup_local.sh
@@ -8,17 +8,17 @@ HOSTNAME=$(hostname -f)
 
 echo $HOSTNAME
 if [ "${1}x" == "homex" ]; then
-	TARBALL="/tmp/home_${DATE}_${HOSTNAME}.tar.xz"
+	TARBALL="/tmp/home_${HOSTNAME}_${DATE}.tar.xz"
 	echo $TARBALL
 	tar cvfJ ${TARBALL} --exclude-backups --exclude "*.iso" /home/
 elif [ "${1}x" == "varx" ]; then
-	TARBALL="/tmp/varetc_${DATE}_${HOSTNAME}.tar.xz"
+	TARBALL="/tmp/varetc_${HOSTNAME}_${DATE}.tar.xz"
 	echo $TARBALL
 	tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/cache/apt/archive /var /etc
 else
 	# Skip any VMs for the full backup.  We should have gotten them in the "home" backup.
-	TARBALL="/tmp/full_${DATE}_${HOSTNAME}.tar.xz"
-	tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /var/cache/apt/archives /
+	TARBALL="/tmp/full_${HOSTNAME}_${DATE}.tar.xz"
+	tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude "/var/cache/apt/archives/*" /
 fi
 
 scp ${TARBALL} 172.16.100.183:/opt/backups/${HOSTNAME}/
