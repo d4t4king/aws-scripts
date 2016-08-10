75b4b9c4b88d8fa3f162cfcf7a7327a580ff337f
d4t4king <dataking@gmail.com>
2016-08-10 10:48:08 -0700

    backup tweaks for live files

--- daily-backup-salt.sh
+++ daily-backup-salt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/salt.dataking.us-${DATE}.tar.xz --directory /root/ /var/www/salt.dataking.us/ /var/www/charlesheselton.com/ /etc/nginx/
+tar cvpfJ /root/salt.dataking.us-${DATE}.tar.xz --exclude-vcs --exclude-backups --exclude="/var/www/salt.dataking.us/db/mem.db" --exclude="/var/www/salt.dataking.us/db/pingtimes.db" --directory /root/ /var/www/salt.dataking.us/ /var/www/charlesheselton.com/ /etc/nginx/
 if [ $? -eq 0 ]; then
 	#scp -P 2222 /root/www.dataking.us-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
 	aws s3 cp /root/salt.dataking.us-${DATE}.tar.xz s3://dk-website-backups/salt.dataking.us/
