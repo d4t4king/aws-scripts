7440a21df77b5d81beef413bcacf681bb2f3a55a
dataking <dataking@gmail.com>
2016-09-06 19:29:12 -0700

    Excluding mail queue dir

--- backup_local.sh
+++ backup_local.sh
@@ -18,21 +18,21 @@ elif [ "${1}x" == "varx" ]; then
 	TARBALL="/tmp/varetc_${DATE}_${HOSTNAME}.tar.xz"
 	echo $TARBALL
 	if [ "${HOSTNAME}" == "luna" ]; then 
-		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/html/mirror --exclude /var/www/html/isos /var /etc
+		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/html/mirror --exclude /var/www/html/isos --exclude /var/nullmailer/queue /var /etc
 	elif [ "${HOSTNAME}" == "mercury.dataking.us" ]; then
-		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/html/mirror --exclude /var/www/html/isos --exclude /var/cache/apt/archive /var /etc
+		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/www/html/mirror --exclude /var/www/html/isos --exclude /var/cache/apt/archive --exclude /var/nullmailer/queue /var /etc
 	else
-		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/cache/apt/archive /var /etc
+		tar cvfJ ${TARBALL} --exclude-backups --exclude-vcs --exclude /var/run --exclude /var/tmp --exclude /var/spool/clientmqueue --exclude /var/cache/apt/archive --exclude /var/nullmailer/queue /var /etc
 	fi
 else
 	# Skip any VMs for the full backup.  We should have gotten them in the "home" backup.
 	TARBALL="/tmp/full_${DATE}_${HOSTNAME}.tar.xz"
 	if [ "${HOSTNAME}" == "luna" ]; then
-		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /s --exclude /var/www/html/mirror /
+		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /s --exclude /var/www/html/mirror --exclude /var/nullmailer/queue /
 	elif [ "${HOSTNAME}" == "oortcloud.dataking.us" ]; then
-		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /s --exclude /var/www/html/mirror --exclude /opt /
+		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /s --exclude /var/www/html/mirror --exclude /opt --exclude /var/nullmailer/queue /
 	else
-		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /var/cache/apt/archives /
+		tar cvfJ ${TARBALL} --exclude /dev --exclude /tmp --exclude /proc --exclude /sys --exclude-vcs --exclude-backups --exclude /media --exclude /mnt --exclude /var/tmp --exclude /run --exclude /var/run --exclude "swe*" --exclude "otw*" --exclude "*.iso" --exclude "*/VirtualBox VMs" --exclude "*/vmware/*" --exclude "*.ova" --exclude /var/cache/apt/archives --exclude /var/nullmailer/queue /
 	fi
 fi
 
