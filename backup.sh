40ddc0e21af1cc4c697d6c33dd8d540e56e385f9
dataking <dataking@gmail.com>
2016-10-27 00:19:19 -0400

    initial commit

---
+++ backup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+HOST=$1
+TYPE=$2
+
+DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+
+if [ "${TYPE}" = "root" ]; then
+	tar cvpfJ /tmp/${TYPE}_${HOST}_${DATE}.tar.xz --exclude-backups /root
+	if [ $? -eq 0 ]; then
+		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
+		if [ $? -eq 0 ]; then
+			rm -f /tmp/${TYPE}_${HOST}_${DATE}.tar.xz
+		else
+			echo "There was a problem with the transfer."
+		fi
+	else
+		echo "There was a problem with the backup."
+	fi
+elif [ "${TYPE}" = "home" ]; then
+	tar cvpfJ /tmp/${TYPE}_${HOST}_${DATE}.tar.xz --exclude-backups --exclude="*.tar.gz" --exclude="*.tar.xz" --exclude="*.iso" /home
+	if [ $? -eq 0 ]; then
+		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
+		if [ $? -eq 0 ]; then
+			rm -vf /tmp/${TYPE}_${HOST}_${DATE}.tar.xz
+		else
+			echo "There was a problem with the transfer."
+		fi
+	else
+		echo "There was a problem with the backup."
+	fi
+elif [ "${TYPE}" = "varetc" ]; then
+	tar cvpfJ /tmp/${TYPE}_${HOST}_${DATE}.tar.xz --exclude-backups --exclude-vcs --exclude="/var/tmp/*" --exclude="/var/run/*" --exclude="/var/cache/*" /etc /var
+	if [ $? -eq 0 ]; then
+		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
+		if [ $? -eq 0 ]; then
+			rm -vf /tmp/${TYPE}_${HOST}_${DATE}.tar.xz
+		else
+			echo "There was a problem with the transfer."
+		fi
+	else
+		echo "There was a problem with the backup."
+	fi
+else
+	tar cvpfJ /tmp/${TYPE}_${HOST}_${DATE}.tar.xz --exclude-backups --exclude-vcs --exclude ="/var/tmp/*" --exclude="/var/run/*" --exclude="/var/cache/*" --exclude="/tmp/*" --exclude="/dev/*" --exclude="/run/*" --exclude="/proc/*" --exclude="*.tar.gz" --exclude="*.tar.xz" /
+	if [ $? -eq 0 ]; then
+		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
+		if [ $? -eq 0 ]; then
+			rm -vf /tmp/${TYPE}_${HOST}_${DATE}.tar.xz
+		else
+			echo "There was a problem with the transfer."
+		fi
+	else
+		echo "There was a problem with the backup."
+	fi
+fi
