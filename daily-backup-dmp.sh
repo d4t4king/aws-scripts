66d2200b8307f27ff875e129c8d22c5805a62cc8
d4t4king <dataking@gmail.com>
2017-06-12 12:00:45 -0700

    config compat with virt hosts

--- daily-backup-dmp.sh
+++ daily-backup-dmp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/www.diegominpin.com-${DATE}.tar.xz --directory /root/ --exclude /usr/share/nginx/html/gallery/resources/cache --exclude /root/www.diegominpin.com/gallery/resources/cache/ /usr/share/nginx/html/ /root/www.diegominpin.com/ /etc/nginx/ /root/img_backup/
+tar cvpfJ /tmp/www.diegominpin.com-${DATE}.tar.xz --exclude /var/www/diegominpin.com/gallery/resources/cache /var/www/diegominpin.com/ /root/img_backup/
 if [ $? -eq 0 ]; then
 	#scp -P 2222 /root/www.diegominpin.com-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
-	aws s3 cp /root/www.diegominpin.com-${DATE}.tar.xz s3://dk-website-backups/diegominpin.com/
+	aws s3 cp /tmp/www.diegominpin.com-${DATE}.tar.xz s3://dk-website-backups/diegominpin.com/
 	if [ $? -eq 0 ]; then
-		rm -f /root/www.diegominpin.com-${DATE}.tar.xz
+		rm -f /tmp/www.diegominpin.com-${DATE}.tar.xz
 	else
 		echo "There was a problem with the transfer."
 	fi
