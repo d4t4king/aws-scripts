0c2dfc435b77101162cb7b879b77be24e006587f
d4t4king <dataking@gmail.com>
2017-06-12 15:58:17 -0700

    updated for /tmp and virt hosting

--- daily-backup-sts.sh
+++ daily-backup-sts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/shermantheshermanator.com-${DATE}.tar.xz --directory /root/ /var/www/shermantheshermanator.com/
+tar cvpfJ /tmp/shermantheshermanator.com-${DATE}.tar.xz --directory /tmp/ /var/www/shermantheshermanator.com/
 if [ $? -eq 0 ]; then
 	#scp -P 2222 /root/www.dataking.us-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
-	aws s3 cp /root/shermantheshermanator.com-${DATE}.tar.xz s3://dk-website-backups/shermantheshermanator.com/
+	aws s3 cp /tmp/shermantheshermanator.com-${DATE}.tar.xz s3://dk-website-backups/shermantheshermanator.com/
 	if [ $? -eq 0 ]; then
-		rm -f /root/shermantheshermanator.com-${DATE}.tar.xz
+		rm -f /tmp/shermantheshermanator.com-${DATE}.tar.xz
 	else
 		echo "There was a problem with the transfer."
 	fi
