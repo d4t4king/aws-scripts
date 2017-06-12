f662b27751e85c6e12c9c3f4d7f894fd549d7bf7
d4t4king <dataking@gmail.com>
2017-06-12 11:18:17 -0700

    configure for local VHost

--- daily-backup-dkt.sh
+++ daily-backup-dkt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/www.dataking.technology-${DATE}.tar.xz --directory /root/ /usr/share/nginx/html/ /root/www.dataking.technology/ /etc/nginx/ /etc/ssl/
+tar cvpfJ /tmp/www.dataking.technology-${DATE}.tar.xz --directory /root/ /usr/share/nginx/html/ /var/www/dataking.technology/ /etc/nginx/ /etc/ssl/
 if [ $? -eq 0 ]; then
 	# old method
 	#scp -P 2222 /root/www.dataking.technology-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
-	aws s3 cp /root/www.dataking.technology-${DATE}.tar.xz s3://dk-website-backups/dataking.technology/
+	aws s3 cp /tmp/www.dataking.technology-${DATE}.tar.xz s3://dk-website-backups/dataking.technology/
 	if [ $? -eq 0 ]; then
-		rm -f /root/www.dataking.technology-${DATE}.tar.xz
+		rm -f /tmp/www.dataking.technology-${DATE}.tar.xz
 	else
 		echo "There was a problem with the transfer."
 	fi
