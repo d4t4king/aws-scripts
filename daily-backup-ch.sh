0862f30f9d6ebeb60d6c8ad5604cfd36080bd7cc
d4t4king <dataking@gmail.com>
2017-06-12 00:25:28 -0700

    updated to /tmp

--- daily-backup-ch.sh
+++ daily-backup-ch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d')
-tar cvpfJ /root/charlesheselton.com-${DATE}.tar.xz --directory /root/ /var/www/charlesheselton.com/
+tar cvpfJ /tmp/charlesheselton.com-${DATE}.tar.xz --directory /root/ /var/www/charlesheselton.com/
 if [ $? -eq 0 ]; then
 	#scp -P 2222 /root/www.dataking.us-${DATE}.tar.xz root@vhome.dataking.us:/media/sf_backups/
-	aws s3 cp /root/charlesheselton.com-${DATE}.tar.xz s3://dk-website-backups/charlesheselton.com/
+	aws s3 cp /tmp/charlesheselton.com-${DATE}.tar.xz s3://dk-website-backups/charlesheselton.com/
 	if [ $? -eq 0 ]; then
-		rm -f /root/charlesheselton.com-${DATE}.tar.xz
+		rm -f /tmp/charlesheselton.com-${DATE}.tar.xz
 	else
 		echo "There was a problem with the transfer."
 	fi
