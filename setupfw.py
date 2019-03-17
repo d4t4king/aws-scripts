83fd9523ee97d3abb267f7ade8b79edc626ff44e
d4t4king <datakin@gmail.com>
2019-03-17 16:38:09 -0700

    python f experimentation; not enough module support - docs suck

--- setupfw.py
+++ setupfw.py
@@ -25,8 +25,11 @@ def get_ifaces():
     ifaces = list()
     with open('/proc/net/dev', 'r') as dev:
         for l in dev:
+            # skip the header row(s)
+            if 'Inter-' in l: continue
+            if 'face' in l: continue
             # skip lo, we'll handle that specifically
-            if 'lo' in l; continue
+            if 'lo' in l: continue
             _if = l.split(':')[0]
             if _if not in ifaces:
                 ifaces.append(_if)
@@ -36,8 +39,24 @@ def main():
     pp = pprint.PrettyPrinter(indent=4)
     tcp_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "TCP")
     udp_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'UDP')
+    log_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'LOGGING')
+    inp_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
+    out_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'OUTPUT')
+    fwd_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'FORWARD')
 
+    # anything coming in from lo, allow
+    rule = iptc.Rule()
+    rule.in_interface = 'lo'
+    t = rule.create_target("ACCEPT")
 
+    pp.pprint(dir(inp_chain.table))
+
+    ifaces = get_ifaces()
+    pp.pprint(ifaces)
+
+    fwd_chain.set_policy(iptc.Policy.DROP)
+
+    
 
 if __name__=='__main__':
     main()
