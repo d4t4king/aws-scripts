e4eea8531ff5ecac3bf36ef11d195ea19732b3c5
d4t4king <dataking@gmail.com>
2021-07-09 23:00:49 -0700

    script add WIP

--- check-authkeys.py
+++ check-authkeys.py
@@ -1,7 +1,25 @@
 
-def main():
-  
+import yaml
 
-if __NAME__=='__MAIN__':
+def get_line_count():
+  afile = '/root/.ssh/authorized_keys'
+  count = len(open(afile).readlines( ))
+  return count
+
+def read_dat():
+  with open('root_auth_keys.dat', 'r') as stream:
+    try:
+      print(yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+      print(exc)
+
+def write_dat():
+  pass
+
+def main():
+  cnt = get_line_count()
+  print("Current count is " + cnt)  
+
+if __name__=='__main__':
   main()
 
