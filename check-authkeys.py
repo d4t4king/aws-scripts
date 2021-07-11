6228fb273305b4d0654d752d6d0217b2bfe54845
d4t4king <dataking@gmail.com>
2021-07-11 09:14:58 -0700

    organize round 3? 4?

--- check-authkeys.py
+++ check-authkeys.py
@@ -1,24 +1,37 @@
 
 import yaml
+import pprint
+
+
+STOREFILE = '/root/root_auth_keys.dat'
+
 
 def get_line_count():
   afile = '/root/.ssh/authorized_keys'
   count = len(open(afile).readlines( ))
   return count
 
+
 def read_dat():
-  with open('root_auth_keys.dat', 'r') as stream:
+  with open(STOREFILE, 'r') as stream:
     try:
       print(yaml.safe_load(stream))
+    #except FileNotFoundError as fnferr:
+      # write out the file (?)
     except yaml.YAMLError as exc:
       print(exc)
 
-def write_dat():
-  pass
+
+def write_dat(yamldict):
+  with open(STOREFILE, 'w') as out:
+    documents = yaml.dump(yamldict, STOREFILE)
+
 
 def main():
+  pp = pprint.PrettyPrinter(indent=4)
+  stored = read_dat()
   cnt = get_line_count()
-  print("Current count is " + cnt)  
+  print("Current count is " + str(cnt))  
 
 if __name__=='__main__':
   main()
