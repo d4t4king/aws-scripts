fe759c1fb6e0815e58e060990e3c33b3a3ee3198
d4t4king <dataking@gmail.com>
2024-07-30 02:42:50 +0000

    Correcting tar compression.

--- sysadmin/backups/backup2aws.sh
+++ sysadmin/backups/backup2aws.sh
@@ -6,7 +6,7 @@ TYPE=$2
 DATE=$(date '+%Y-%m-%d-%H-%M-%S')
 
 if [ "${TYPE}" = "root" ]; then
-	tar cpf /tmp/${TYPE}_${HOST}_${DATE}.tar --exclude-backups /root | xz -9 /tmp/${TYPE}_${HOST}_${DATE}.tar
+	tar cpf /tmp/${TYPE}_${HOST}_${DATE}.tar --exclude-backups /root && xz -9 /tmp/${TYPE}_${HOST}_${DATE}.tar
 	if [ $? -eq 0 ]; then
 		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
 		if [ $? -eq 0 ]; then
@@ -18,7 +18,7 @@ if [ "${TYPE}" = "root" ]; then
 		echo "There was a problem with the backup."
 	fi
 elif [ "${TYPE}" = "home" ]; then
-	tar cpf /tmp/${TYPE}_${HOST}_${DATE}.tar --exclude-backups --exclude="*.tar.gz" --exclude="*.tar.xz" --exclude="*.iso" /home | xz -9 /tmp/${TYPE}_${HOST}_${DATE}.tar
+	tar cpf /tmp/${TYPE}_${HOST}_${DATE}.tar --exclude-backups --exclude="*.tar.gz" --exclude="*.tar.xz" --exclude="*.iso" /home && xz -9 /tmp/${TYPE}_${HOST}_${DATE}.tar
 	if [ $? -eq 0 ]; then
 		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
 		if [ $? -eq 0 ]; then
@@ -30,7 +30,7 @@ elif [ "${TYPE}" = "home" ]; then
 		echo "There was a problem with the backup."
 	fi
 elif [ "${TYPE}" = "varetc" ]; then
-	tar cpf /tmp/${TYPE}_${HOST}_${DATE}.tar --exclude-backups --exclude-vcs --exclude="/var/tmp" --exclude="/var/tmp/*" --exclude="/var/run" --exclude="/var/run/*" --exclude="/var/cache" --exclude="/var/cache/*" --exclude="/var/lib/lxcfs" --exclude="/var/lib/lxcfs/*" --exclude="/var/spool" --exclude="/var/spool/*" --exclude="/var/agentx/master" --exclude="/var/lib/clamav" --exclude="/var/lib/lxd/unix.socket" --exclude="/var/lib/mysql/ib_logfile0" /etc /var | xz -9 /tmp/${TYPE}_${HOST}_${DATE}.tar
+	tar cpf /tmp/${TYPE}_${HOST}_${DATE}.tar --exclude-backups --exclude-vcs --exclude="/var/tmp" --exclude="/var/tmp/*" --exclude="/var/run" --exclude="/var/run/*" --exclude="/var/cache" --exclude="/var/cache/*" --exclude="/var/lib/lxcfs" --exclude="/var/lib/lxcfs/*" --exclude="/var/spool" --exclude="/var/spool/*" --exclude="/var/agentx/master" --exclude="/var/lib/clamav" --exclude="/var/lib/lxd/unix.socket" --exclude="/var/lib/mysql/ib_logfile0" /etc /var && xz -9 /tmp/${TYPE}_${HOST}_${DATE}.tar
 	if [ $? -eq 0 ]; then
 		aws s3 cp /tmp/${TYPE}_${HOST}_${DATE}.tar.xz s3://dk-website-backups/${HOST}/
 		if [ $? -eq 0 ]; then
