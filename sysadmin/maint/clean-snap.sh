b114f2c2a2215f16e5a0c23ae972277fb260861d
d4t4king <dataking@gmail.com>
2024-08-05 09:12:08 -0700

    typo

--- sysadmin/maint/clean-snap.sh
+++ sysadmin/maint/clean-snap.sh
@@ -5,7 +5,7 @@ echo "Clean up the snap cache by removing disabled snaps."
 # CLOSE ALL SNAPS BEFORE RUNNING THIS!
 
 set -eu
-snap list --all | awk '/disabled/{print $1, $3' |
+snap list --all | awk '/disabled/{print $1, $3}' |
 	while read snapname revision; do
 		snap remove "$snapname" --revision="$revision"
 	done
