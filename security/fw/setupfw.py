9c91a00b16dfea5dfe53c3f49abf30c846a90c70
d4t4king <dataking@gmail.com>
2025-12-06 15:28:21 -0500

    WIP

--- security/fw/setupfw.py
+++ security/fw/setupfw.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Set up a basic firewall. 
 
@@ -8,11 +8,22 @@ those ports and services.
 
 It will also attempt to set up some very basic rules
 for a simple, stateful iptables firewall.
-
 """
 
 import iptc
 import pprint
+import argparse
+from termcolor import cprint
+
+def is_root() -> bool:
+    """
+    Check if the script is being run as root.
+
+    Returns:
+        bool: True if running as root, False otherwise.
+    """
+    import os
+    return os.geteuid() == 0
 
 def get_ifaces():
     """
@@ -36,7 +47,23 @@ def get_ifaces():
     return ifaces
 
 def main():
+    # if we're not rot, we can't do much so check then exit if not root
+    if not is_root():
+        cprint("ERROR: This script must be run as root.", "red")
+        print("Example: sudo python3 setupfw.py")
+        exit(1)
+    # allow for prettyy printing of objects
     pp = pprint.PrettyPrinter(indent=4)
+
+    # handle some arguments
+    parser = argparse.ArgumentParser(description="Set up a basic firewall.")
+    vqd = parser.add_mutually_exclusive_group()
+    vqd.add_argument('-v', '--verbose', dest="verbose", required=False, action="store_true", help="Increase output verbosity.")
+    vqd.add_argument('-q', '--quiet', dest="quiet", required=False, action="store_true", help="Suppress output verbosity.")
+    vqd.add_argument('--debug', dest="debug", required=False, action="store_true", help="Enable debug output verbosity.")
+    parser.add_argument('-F', '--flush', dest="flush", required=False, action="store_true", help="Flush existing iptables rules before applying new ones.")
+    args = parser.parse_args()
+
     tcp_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "TCP")
     udp_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'UDP')
     log_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'LOGGING')
@@ -56,7 +83,6 @@ def main():
 
     fwd_chain.set_policy(iptc.Policy.DROP)
 
-    
 
 if __name__=='__main__':
     main()
