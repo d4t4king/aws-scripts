c6af63a8895f3d2215eee6d6a3a2ecb6e344ac8b
d4t4king <dataking@gmail.com>
2026-03-28 17:09:08 -0400

    checking type

--- security/logs/logdiver.py
+++ security/logs/logdiver.py
@@ -49,7 +49,8 @@ def get_country_code_from_ip(ipaddress: IP.IPv4Address | IP.IPv6Address, authtok
             'Content-Type': 'application/json'
         }
         resp = requests.get(url, headers=headers)
-        r_json = json.dumps(resp.json())
+        r_json = resp.json().decode("utf-8")
+        print(f"INFO :: r_json is of type {str(type(r_json))}")
         pp.pprint(r_json)
         # pp.pprint(json.dumps(resp.json()))
         # pp.pprint(resp.content)
